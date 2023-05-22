6debbb15bfc347877229c51c9cbaeb4d44c5d881
Hem-Mu <kimmim1223@gmail.com>
2023-05-22 16:55:40 +0900

    23-05-22

--- 00.main.tf
+++ 00.main.tf
@@ -1,47 +1,5 @@
-#git remote add <alias> <git path>
-
-# git remote show
-# git init
-# git add -p <file>
-# git commit -m "<message>"
-# git push <remote>
-# git push <remote> --force
-
-# git rm --cached <폴더> -r
-##
-
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = var.region
+  region     = var.region
 }
-
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "terraform-up-and-running-locks"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# } # dynamodb table create
-
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "hemmu"
-#   # Enable versioning so we can see the full revision history of our state files
-#   versioning {
-#     enabled = true
-#   }
-#   # Enable server-side encryption by default
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-#   force_destroy = true
-#   depends_on = [
-#     aws_dynamodb_table.terraform_locks
-#   ]
-# } # s3 create
--- 01.backend.tf
+++ 01.backend.tf
@@ -1,27 +1,15 @@
+# terraform {
+#   backend "local" {
+#     path = "./terraform.tfstate"
+#   } # backend 변경 전 destroy 권장
+# } 
+
 terraform {
-  backend "local" {
-    path = "./terraform.tfstate"
-  } # backend 변경 전 destroy 권장
-} 
-
-/* terraform {
-
   backend "s3" {
-    bucket         = "art-id-apnortheast2-tfstate" # bucket name
-    key            = "terrafom/terraform.tfstate" # tfstate file object name
+    bucket         = "minwook-terraform-state-bucket" # bucket name
+    key            = "terraform.tfstate" # tfstate file save path
     region         = "ap-northeast-2"
     encrypt        = true
-    dynamodb_table = "terraform-lock" # DDB name
+    dynamodb_table = "terraform-lock" # DDB name.
   }
-} */
-
-/* resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-lock"
-  hash_key       = "LockID"
-  billing_mode   = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-} */
+}
